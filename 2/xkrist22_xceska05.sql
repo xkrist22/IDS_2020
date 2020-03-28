5d05b3e9fe01de1c132bbebe612a38b2b7e51e00
Kyperas <ceskape@seznam.cz>
2020-03-28 18:23:11 +0100

    adding inserts into initial file

--- 2/xkrist22_xceska05.sql
+++ 2/xkrist22_xceska05.sql
@@ -141,3 +141,135 @@ CREATE TABLE offer (
     CONSTRAINT offer_beer_fk FOREIGN KEY(beer_id) REFERENCES beer(id) ON DELETE SET NULL,
     CONSTRAINT offer_pub_fk FOREIGN KEY(pub_id) REFERENCES pub(id) ON DELETE SET NULL
 );
+
+
+/*
+---------TEMPLATY PRO INSERTY DO JEDNOTLIVÝCH TABULEK-------
+
+INSERT INTO account (id, email, city, street, house) values ('', '', '', '', '');
+
+INSERT INTO pub (id, name) values ('', '');
+
+INSERT INTO person (id, name, surname, birth_date) values ('', '', '');
+
+INSERT INTO brewery (id, name, established_date) values ('', '', '');
+
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('', '', '', '');
+
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('', '', '', '', '');
+
+INSERT INTO malt (id, color, extract, sugar_volume) values ('', '', '', '');
+
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('', '', '', '', '', '', '', '', '', '', '', '');
+
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('', '', '', '', '');
+
+INSERT INTO pub_rating (pub_id, person_id, service, interior, result) values ('', '', '', '', '');
+
+INSERT INTO offer (pub_id, beer_id, price) values ('', '', '');
+*/
+
+
+INSERT INTO account (id, email, city, street, house) values ('1', 'lx@svi.cz', 'Svitavy', 'Safarikova', '11');
+INSERT INTO account (id, email, city, street, house) values ('3', 'standa@seznam.cz', 'Polička', 'Nadrazni', '3');
+INSERT INTO account (id, email, city, street, house) values ('2', 'dita@gmail.com', 'Svitavy', 'Dlouhá', '32');
+
+
+INSERT INTO account (id, email, city, street, house) values ('4', 'pavel@svi.cz', 'Svitavy', 'Safarikova', '11');
+INSERT INTO account (id, email, city, street, house) values ('5', 'honza@seznam.cz', 'Polička', 'Nadrazni', '23');
+INSERT INTO account (id, email, city, street, house) values ('6', 'kamil@gmail.com', 'Brno', 'Strmá', '4322');
+
+
+INSERT INTO account (id, email, city, street, house) values ('7', 'maja@svi.cz', 'Svitavy', 'Strmá', '11');
+INSERT INTO account (id, email, city, street, house) values ('8', 'david@seznam.cz', 'Polička', 'Nadrazni', '53');
+INSERT INTO account (id, email, city, street, house) values ('9', 'anet@gmail.com', 'Praha', 'Dlouhá', '42');
+
+
+INSERT INTO account (id, email, city, street, house) values ('10', 'emily@svi.cz', 'Svitavy', 'Safarikova', '111');
+INSERT INTO account (id, email, city, street, house) values ('11', 'denis@seznam.cz', 'Olomouc', 'Skácelova', '73');
+INSERT INTO account (id, email, city, street, house) values ('12', 'steave@gmail.com', 'Česká Třebová', 'Příční', '4');
+
+
+INSERT INTO pub (id, name) values ('2', 'U Ditky');
+INSERT INTO person (id, name, surname, birth_date) values (1, 'Petr', ' Češka', TO_DATE('1999-03-25','YYYY-MM-DD'));
+INSERT INTO brewery (id, name, established_date) values ('3', 'Na kopečku', TO_DATE('2009-06-21','YYYY-MM-DD'));
+
+
+INSERT INTO person (id, name, surname, birth_date) values (4, 'Pavel', ' Staša', TO_DATE('1999-12-23','YYYY-MM-DD'));
+INSERT INTO person (id, name, surname, birth_date) values (5, 'Honza', ' Čáp', TO_DATE('1989-03-12','YYYY-MM-DD'));
+INSERT INTO person (id, name, surname, birth_date) values (6, 'Kamil', ' Muller', TO_DATE('1998-01-04','YYYY-MM-DD'));
+
+
+INSERT INTO pub (id, name) values ('7', 'U Ditky');
+INSERT INTO pub (id, name) values ('8', 'Černá Kočka');
+INSERT INTO pub (id, name) values ('9', 'Slavie');
+
+
+INSERT INTO brewery (id, name, established_date) values ('10', 'Na kopečku', TO_DATE('2009-05-25','YYYY-MM-DD'));
+INSERT INTO brewery (id, name, established_date) values ('11', 'Staropramen', TO_DATE('1989-04-22','YYYY-MM-DD'));
+INSERT INTO brewery (id, name, established_date) values ('12', 'Bernardu', TO_DATE('2019-08-17','YYYY-MM-DD'));
+
+
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('1', 'cocoa', '4', '6');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('2', 'coffee', '3', '7');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('3', 'orange', '5', '6');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('5', 'citrus', '4', '8');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('6', 'cocoa', '4', '6');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('7', 'coffee', '3', '7');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('8', 'orange', '2', '5');
+INSERT INTO hop (id, aroma, bitterness, alpha_acid_volume) values ('9', 'citrus', '4', '1');
+
+
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('1', 'liquid', '2', '1', '7');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('2', 'solid', '4', '7', '2');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('3', 'gas', '2', '3', '7');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('5', 'liquid', '8', '5', '1');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('6', 'liquid', '2', '1', '7');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('7', 'solid', '4', '6', '8');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('8', 'gas', '1', '5', '5');
+INSERT INTO yeast (id, state, proteins_volume, sugar_volume, vitamins_volume) values ('9', 'liquid', '8', '5', '1');
+
+
+INSERT INTO malt (id, color, extract, sugar_volume) values ('1', 'black', 'coffee', '7');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('2', 'brown', 'caramel', '6');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('3', 'yellow', 'lemon', '7');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('4', 'orange', 'orange', '7');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('5', 'black', 'coffee', '7');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('6', 'brown', 'caramel', '6');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('7', 'yellow', 'lemon', '7');
+INSERT INTO malt (id, color, extract, sugar_volume) values ('8', 'orange', 'orange', '7');
+
+
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('1', 'yellow', 'Mouřenín', 'dark', '13', 'inner', '428', 'orange', '11', '1', '2', '3');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('2','black', 'Oliver', 'semi-dark', '12', 'inner', '473', 'banana', '12', '2', '3', '1');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('3','yellow', 'Jedenáctka', 'semi-dark', '11', 'inner', '411', 'simple', '10', '3', '1', '4');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('4','black', 'Raven Dark', 'light', '13', 'outer', '453', 'simple', '11', '5', '8', '4');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('5','brown', 'Raven12', 'light', '12', 'inner', '523', 'banana', '12', '5', '7', '1');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('6','brown', 'Svitavan', 'light', '13', 'inner', '423', 'orange', '12', '6', '1', '3');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('7','brown', 'Kovář', 'dark', '14', 'outer', '421', 'cocoa', '11', '7', '3', '2');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('8','yellow', 'Sloupec', 'light', '12', 'inner', '476', 'orange', '3', '1', '1', '8');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('9','yellow', 'Raven', 'dark', '10', 'inner', '444', 'coffee', '10', '8', '5', '8');
+INSERT INTO beer (id, name, color, type, alcohol_volume, fermentation_type, bitterness, savor, brewery_id, hop_id, yeast_id, malt_id) values ('10','yellow', 'red vine', 'light', '10', 'inner', '463', 'orange', '3', '3', '1', '8');
+
+
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('1', '5', '4', '4', '4');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('2', '6', '3', '3', '1');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('3', '6', '2', '5', '1');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('4', '4', '3', '4', '2');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('5', '5', '4', '1', '2');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('6', '4', '5', '2', '3');
+INSERT INTO beer_rating (beer_id, person_id, taste, foam, smell) values ('7', '4', '4', '3', '2');
+
+
+INSERT INTO pub_rating (pub_id, person_id, service, interior, result) values ('7', '1', '2', '2', '4');
+INSERT INTO pub_rating (pub_id, person_id, service, interior, result) values ('8', '1', '3', '2', '3');
+INSERT INTO pub_rating (pub_id, person_id, service, interior, result) values ('8', '5', '4', '5', '1');
+INSERT INTO pub_rating (pub_id, person_id, service, interior, result) values ('9', '6', '5', '3', '5');
+
+
+INSERT INTO offer (pub_id, beer_id, price) values ('7', '1', '27');
+INSERT INTO offer (pub_id, beer_id, price) values ('7', '3', '25');
+INSERT INTO offer (pub_id, beer_id, price) values ('8', '2', '22');
+INSERT INTO offer (pub_id, beer_id, price) values ('8', '1', '34');
+INSERT INTO offer (pub_id, beer_id, price) values ('8', '5', '33');
+INSERT INTO offer (pub_id, beer_id, price) values ('9', '7', '41');
