5af48ea572eae6aca45605a7c069a708c897c84c
Kyperas <ceskape@seznam.cz>
2020-04-09 21:20:35 +0200

    added part 3 basic file

---
+++ 3/xkrist22_xceska05.sql
@@ -0,0 +1,11 @@
+--SQL skript obsahující dotazy SELECT musí obsahovat konkrétně alespoň dva dotazy využívající spojení dvou tabulek, jeden využívající spojení tří tabulek, 
+--dva dotazy s klauzulí GROUP BY a agregační funkcí, jeden dotaz obsahující predikát EXISTS a jeden dotaz s predikátem IN s vnořeným selectem (nikoliv IN s množinou konstantních dat). 
+--U každého z dotazů musí být (v komentáři SQL kódu) popsáno srozumitelně, jaká data hledá daný dotaz (jaká je jeho funkce v aplikaci).
+--------------------
+-- 2x spojení 2 tabulek
+-- 1x spojení 3 tabulek
+-- 2x GROUP BY + agregační fce
+-- 1x EXISTS
+-- 1x IN + vnořený select
+-- 7x popisky k čemu select spouží
+
