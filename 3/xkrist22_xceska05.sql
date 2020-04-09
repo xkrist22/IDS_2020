68bef1edc0701a34237ff310d7ecbebb34e8aa5f
Kyperas <ceskape@seznam.cz>
2020-04-09 22:01:43 +0200

    first select

--- 3/xkrist22_xceska05.sql
+++ 3/xkrist22_xceska05.sql
@@ -2,10 +2,12 @@
 --dva dotazy s klauzulí GROUP BY a agregační funkcí, jeden dotaz obsahující predikát EXISTS a jeden dotaz s predikátem IN s vnořeným selectem (nikoliv IN s množinou konstantních dat). 
 --U každého z dotazů musí být (v komentáři SQL kódu) popsáno srozumitelně, jaká data hledá daný dotaz (jaká je jeho funkce v aplikaci).
 --------------------
--- 2x spojení 2 tabulek
+-- 2x spojení 2 tabulek [1/2]
 -- 1x spojení 3 tabulek
 -- 2x GROUP BY + agregační fce
 -- 1x EXISTS
 -- 1x IN + vnořený select
--- 7x popisky k čemu select spouží
+-- 7x popisky k čemu select spouží [1/7]
 
+-- Nová hospoda ve Svitavách chce zaslat nabídku akcí místním uživatelům systému. [zobraz jméno a kontakt uživatelů žijících ve Svitavách.]
+SELECT person.name AS jméno, person.surname AS příjmení, account.email AS kontakt FROM person JOIN account ON person.id = account.id WHERE account.city='Svitavy';
