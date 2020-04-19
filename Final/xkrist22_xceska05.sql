26382dc39719fda9aedd6ffc62a34ea63da9b60b
Kyperas <ceskape@seznam.cz>
2020-04-19 19:30:34 +0200

    trying triggers

--- Final/xkrist22_xceska05.sql
+++ Final/xkrist22_xceska05.sql
@@ -9,7 +9,23 @@ END;
 
 CREATE OR REPLACE TRIGGER test
 BEFORE DELETE ON offer
-FOR EACH ROW
+FOR EACH ROWł
 BEGIN
 DELETE FROM pub_rating WHERE pub_id = :old.id;
+END;
+
+CREATE OR REPLACE TRIGGER pub1
+BEFORE DELETE ON pub
+FOR EACH ROW
+BEGIN
+DELETE FROM offer where pub_id = 7;ł
+DELETE FROM pub_rating where pub_id = 7;
+END;
+
+CREATE OR REPLACE TRIGGER tst
+BEFORE DELETE ON beer
+FOR EACH ROW
+BEGIN
+DELETE FROM beer_rating WHERE beer_id = :old.id;
+DELETE FROM offer WHERE beer_id = :old.id;
 END;
